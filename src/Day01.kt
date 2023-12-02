f18e3ee9627855ebccebe371bee1421efbf382a9
Pranjal Vachaspati <pr@nj.al>
2023-12-02 14:05:52 -0500

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,46 @@
 fun main() {
+
+    val digitsmap = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+    val digitsIndivRegex = digitsmap.keys.map { it.toRegex() }
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val vals = input.map {
+            it.filter { it.isDigit() }
+        }.also { println(it) }.map { it.first().digitToInt() * 10 + it.last().digitToInt() }
+
+        return vals.sum()
     }
 
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val vals = input.map { str ->
+            val writtenDigits = digitsIndivRegex.flatMap { it.findAll(str) }
+            println(str)
+            writtenDigits.fold(str) { s, match ->
+                s.replaceRange(
+                    match.range.first, match.range.first + 1, digitsmap[match.value].toString()
+                )
+            }.also { println(it) }.filter { it.isDigit() }
+                .let { it.first().digitToInt() * 10 + it.last().digitToInt() }
+        }
+        return vals.sum()
     }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
